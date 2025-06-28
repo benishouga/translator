3611c3e89f36c37a0a9c4308fba23024b276eebb
Akito Moriki <benishouga@gmail.com>
2025-06-28 16:25:20 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="m-2 p-2 size-40 rounded bg-blue-300">01</div>
+      <div className="m-2 p-2 size-10 rounded bg-blue-300">02</div>
+      <div className="m-2 p-2 size-40 rounded bg-blue-300">03</div>
+    </div>
+  );
+}
+
+export default App;
